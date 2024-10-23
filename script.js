56090b2b8074b2f27ada4d8d66c10df27f1888f2
DaviCarvalhoo <davicarvalhotech@gmail.com>
2024-10-23 17:45:14 -0300

    add `local storage` and `edit card apt`

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 let apts = {
-  blocoA: [
-    { id: 101, morador: "Davi", color: "full" },
-    { id: 102, morador: "Davi Carvalho Alves", color: "default" },
-  ],
-  blocoB: [
-    { id: 201, morador: "Davi", color: "medium" },
-  ],
-  blocoC: [
-    { id: 301, morador: "Davi", color: "low" },
-  ]
+  blocoA: [],
+  blocoB: [],
+  blocoC: []
 };
 
-let cBloco = 'blocoA'; 
+let cBloco = 'blocoA';
+
+const saveToLocalStorage = () => {
+  localStorage.setItem('apartamentos', JSON.stringify(apts));
+};
+const loadFromLocalStorage = () => {
+  const storedApts = localStorage.getItem('apartamentos');
+  if (storedApts) {
+    apts = JSON.parse(storedApts);
+  }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadFromLocalStorage();  
+  const defaultButton = document.querySelector('.btn.bloco.active');
+  showApts(cBloco, defaultButton);  o
+});
+
+ 
 
 function showApts(bloco, element) {
   document.querySelectorAll('.btn.bloco').forEach(button => {
@@ -33,7 +44,9 @@ function addAptsDOM(apartamento, container) {
   aptElement.className = `apt-card ${apartamento.color}`; 
   aptElement.innerHTML = `<span class="text">${apartamento.morador} -<b> Apartamento ${apartamento.id}</b></span>
   <img src="./imgs/${apartamento.color}.svg" alt="${apartamento.color}">`;
-  container.appendChild(aptElement); 
+   
+  aptElement.onclick = () => openEditModal(apartamento)
+  container.appendChild(aptElement);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -44,9 +57,18 @@ document.addEventListener('DOMContentLoaded', () => {
 const openAddModal = () => {
   document.getElementById('modal').classList.add('active');
 }
+const openEditModal = (apartamento) => {
+  document.getElementById('modal-edit').classList.add('active');
+  document.getElementById('id').value = apartamento.id;
+  document.getElementById('nome').value = apartamento.morador;
+  document.getElementById('color').value = apartamento.color;
+}
 
 const closeModal = () => {
   document.getElementById('modal').classList.remove('active');
+}
+const closeModalEdit = () => {
+  document.getElementById('modal-edit').classList.remove('active');
 }
 
 document.getElementById('add-apt-form').addEventListener('submit', function (e) {
@@ -64,6 +86,7 @@ document.getElementById('add-apt-form').addEventListener('submit', function (e) 
 
   if (selectedBloco in apts) {
     apts[selectedBloco].push(novoApartamento); 
+    saveToLocalStorage();
     showApts(selectedBloco, document.querySelector(`button[onclick="showApts('${selectedBloco}', this)"]`));
   } else {
     console.error(`Bloco ${selectedBloco} não encontrado.`);
@@ -71,3 +94,43 @@ document.getElementById('add-apt-form').addEventListener('submit', function (e) 
 
   closeModal();
 });
+
+
+const removeApt = () => {
+  const aptId = document.getElementById('id').value;
+  const apartamentosBloco = apts[cBloco];
+  const aptIndex = apartamentosBloco.findIndex(apt => apt.id == aptId);
+  if (aptIndex !== -1) {
+    apartamentosBloco.splice(aptIndex, 1);
+    showApts(cBloco, document.querySelector(`button[onclick="showApts('${cBloco}', this)"]`));
+    saveToLocalStorage();
+    closeModalEdit(); 
+  } else {
+    alert('Apartamento não encontrado!');
+  }
+
+
+}
+
+document.getElementById('edit-apt-form').addEventListener('submit', function(e){
+  e.preventDefault()
+
+  const aptId = document.getElementById('id').value
+  const aptNome = document.getElementById('nome').value
+  const color = document.getElementById('color').value
+
+  for (const bloco in apts) {
+    const aptIndex = apts[bloco].findIndex(apt => apt.id == aptId);
+    if (aptIndex !== -1) {
+      apts[bloco][aptIndex].morador = aptNome;
+      apts[bloco][aptIndex].color = color;
+      break; 
+    }
+  }
+
+  showApts(cBloco, document.querySelector(`button[onclick="showApts('${cBloco}', this)"]`));
+  saveToLocalStorage();
+  closeModalEdit()
+})
+
+
